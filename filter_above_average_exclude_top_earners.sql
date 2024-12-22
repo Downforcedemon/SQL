5d154a588b0193d5c04053cd48c43765ab3e7234
Downforcedemon <114124700+Downforcedemon@users.noreply.github.com>
2024-12-22 15:10:56 -0800

    Update filter_above_average_exclude_top_earners.sql

--- filter_above_average_exclude_top_earners.sql
+++ filter_above_average_exclude_top_earners.sql
@@ -19,3 +19,34 @@ WHERE s.year = 2013
       -- Filter to only include the top 5 earners for each job title
       WHERE rank <= 5
   );
+
+
+-- ALTERNATIVe
+SELECT main.employeename
+FROM sf_public_salaries main
+JOIN (
+    SELECT 
+        jobtitle,
+        AVG(totalpay) AS avg_pay
+    FROM sf_public_salaries
+    WHERE year = 2013
+    GROUP BY jobtitle
+) aves 
+ON main.jobtitle = aves.jobtitle
+   AND main.totalpay > aves.avg_pay
+WHERE main.employeename IN (
+    SELECT employeename
+    FROM (
+        SELECT 
+            employeename,
+            jobtitle,
+            totalpay,
+            RANK() OVER (
+                PARTITION BY jobtitle
+                ORDER BY totalpay DESC
+            ) AS rk
+        FROM sf_public_salaries
+        WHERE year = 2013
+    ) sq
+    WHERE rk > 5
+);
